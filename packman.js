a525c81583a8d84c99b6bbb4a4c2094caab3e1cc
Patrick Brosset <patrick.brosset@amadeus.com>
2012-04-29 13:20:28 +0200

    default config file .packman

--- packman.js
+++ packman.js
@@ -25,43 +25,50 @@ var fs = require("fs");
 require("./libs/logger.js");
 
 var argv = require('optimist')
-    .usage('Package files.\nUsage: $0 -c path/to/myconfig.json')
-    .demand('c')
+    .usage('Usage:\n  packman')
     .alias('c', 'config')
-    .describe('c', 'Path to the config file to use')
+    .describe('c', 'Path to the config file to use (defaults to .packman in the current working dir)')
+    .default('c', './.packman')
     .alias('l', 'logging')
     .describe('l', 'Logging level (only errors = 1, also warnings = 2, also info = 3, also debug = 4)')
     .default('l', 1)
+    .alias('h', 'help')
+    .boolean('h')
+    .describe('h', 'Displays this message')
     .argv
 ;
 
-logger.level = argv.l;
+if(argv.h) {
+    console.log(require('optimist').help());
+} else {
+    logger.level = argv.l;
 
-var config = require("./libs/config.js").get(argv.c)
+    var config = require("./libs/config.js").get(argv.c)
 
-if(config !== null && Object.keys(config.packages).length > 0) {
-    var isEnvReady = require("./libs/env.js").prepare(config.destination, config.eraseIfExists);
+    if(config !== null && Object.keys(config.packages).length > 0) {
+        var isEnvReady = require("./libs/env.js").prepare(config.destination, config.eraseIfExists);
 
-    if(isEnvReady) {
-        var allSourceFiles = require("./libs/finder.js").getAllSourceFiles(config.source);
+        if(isEnvReady) {
+            var allSourceFiles = require("./libs/finder.js").getAllSourceFiles(config.source);
 
-        if(allSourceFiles.length > 0) {
-            var resolvedPackages = require("./libs/clone.js").clone(config.packages);
-            resolvedPackages = require("./libs/resolver.js").resolveFilePaths(resolvedPackages, allSourceFiles);
-            config.resolvedPackages = resolvedPackages;
+            if(allSourceFiles.length > 0) {
+                var resolvedPackages = require("./libs/clone.js").clone(config.packages);
+                resolvedPackages = require("./libs/resolver.js").resolveFilePaths(resolvedPackages, allSourceFiles);
+                config.resolvedPackages = resolvedPackages;
 
-            logger.logInfo("Getting started with: source=" + config.source + ", destination=" + config.destination + ", eraseIfExists=" + config.eraseIfExists);
+                logger.logInfo("Getting started with: source=" + config.source + ", destination=" + config.destination + ", eraseIfExists=" + config.eraseIfExists);
 
-            var merger = require("./libs/merger.js");
+                var merger = require("./libs/merger.js");
 
-            merger.merge(config, function() {
-                console.log("");
-                var time = ((new Date().getTime()) - startTime)/1000;
-                console.log((" packman did it again! Have a great day! (" + time + " sec)").yellow.bold);
-            });
+                merger.merge(config, function() {
+                    console.log("");
+                    var time = ((new Date().getTime()) - startTime)/1000;
+                    console.log((" packman did it again! Have a great day! (" + time + " sec)").yellow.bold);
+                });
+            }
+        } else {
+            logger.logWarning("packman could not prepare the destination directory, there's an existing file at " + config.destination);
+            logger.logWarning("Make sure you configure the proper destination directory path or set the 'eraseIfExists' config flag to true.");
         }
-    } else {
-        logger.logWarning("packman could not prepare the destination directory, there's an existing file at " + config.destination);
-        logger.logWarning("Make sure you configure the proper destination directory path or set the 'eraseIfExists' config flag to true.");
     }
 }
