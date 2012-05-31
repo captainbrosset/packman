ae46d1958997d7f55d4707e508067bdd2f78cd0b
Patrick Brosset <patrick.brosset@amadeus.com>
2012-05-31 19:13:02 +0200

    testing dir walker

--- libs/test.js
+++ libs/test.js
@@ -1,19 +1,18 @@
 var fs = require("fs");
 
-function walkDir(directory, handler) {
+function walkDir(directory, visitor) {
     var files = fs.readdirSync(directory);
     for(var i = 0, l = files.length; i < l; i ++) {
         var fullFilePath = directory + "/" + files[i];
         var stats = fs.statSync(fullFilePath);
-        if(stats.isFile()) {
-            handler(fullFilePath);
-        } else if(stats.isDirectory()) {
-            walkDir(fullFilePath, handler);
+        if(stats.isDirectory()) {
+            walkDir(fullFilePath, visitor);
         }
+        visitor(fullFilePath, stats);
     }
 }
 
 
-walkDir(".", function(file) {
-    console.log(file);
+walkDir(".", function(file, stats) {
+    console.log(file + " is a " + (stats.isDirectory() ? "dir" : "file"));
 });
