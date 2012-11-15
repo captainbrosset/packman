9d11ab851ad59f2b9eb6de3fbba01365b22313b6
Patrick Brosset <patrick.brosset@amadeus.com>
2012-11-15 21:53:37 +0100

    Stats usage example

--- test/missingfile.js
+++ test/missingfile.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var fs = require('fs');
 
-describe("packman stack exhaust test", function() {
+describe("packman missing file handling test", function() {
   it("should crash on missing file", function(done) {
     require("child_process").exec("node packman.js -l 4 -c test/missingfileko.yaml", function(error, stdout, stderr) {
       assert.ok(!!error && error.code === 1);
