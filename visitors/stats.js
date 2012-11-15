cb86ff1dd9bf86c337ba4b1255ca3b44f3e33b86
Patrick Brosset <patrick.brosset@amadeus.com>
2012-11-15 21:43:23 +0100

    Introducing a new Stats visitor
    The stats visitor simply generates an HTML report file about the source
    folder, giving metrics about the number of lines of codes, number of
    files, and types of files

---
+++ visitors/stats.js
@@ -0,0 +1,96 @@
+var fs = require('fs');
+var bind = require('bind');
+
+module.exports.onFileContent = function(callback, config, fileObject) {
+  var lineNb = fileObject.content.split('\n').length;
+
+  if(!fileObject.packageFile.stats) {
+    fileObject.packageFile.stats = [];
+  }
+
+  fileObject.packageFile.stats.push({
+    path: fileObject.path,
+    nb: lineNb
+  });
+
+  fileObject.content = '';
+  callback();
+};
+
+function getFileSizes(stats) {
+  var below10 = 0;
+  var below100 = 0;
+  var below500 = 0;
+  var below1000 = 0;
+  var above = 0;
+
+  for(var i = 0; i < stats.length; i++) {
+    if(stats[i].nb < 10) {
+      below10++;
+    } else if(stats[i].nb < 100) {
+      below100++;
+    } else if(stats[i].nb < 500) {
+      below500++;
+    } else if(stats[i].nb < 1000) {
+      below1000++;
+    } else {
+      above++;
+    }
+  }
+
+  return [
+    {max: 10, nb: below10},
+    {max: 100, nb: below100},
+    {max: 500, nb: below500},
+    {max: 1000, nb: below1000},
+    {max: Infinity, nb: above},
+  ];
+};
+
+function getFileTypes(stats) {
+  var exts = {};
+
+  for(var i = 0; i < stats.length; i++) {
+    var extPos = stats[i].path.lastIndexOf('.');
+    var ext = stats[i].path.substring(extPos + 1);
+    if(!exts[ext]) {
+      exts[ext] = 0;
+    }
+    exts[ext]++;
+  }
+
+  var fileTypes = [];
+  for(var type in exts) {
+    fileTypes.push({type: type, nb: exts[type]});
+  }
+  return fileTypes;
+};
+
+module.exports.onPackageEnd = function(callback, config, packageFileObject) {
+  var now = new Date();
+  packageFileObject.path = 'Stats ' + now + '.html';
+
+  var stats = packageFileObject.stats,
+    totalLines = 0;
+
+  for(var i = 0; i < stats.length; i ++) {
+    totalLines += stats[i].nb;
+  }
+
+  stats.sort(function(a, b) {
+    return b.nb - a.nb;
+  });
+
+  bind.toFile('./visitors/stats-tpl.html', {
+    sourceFolder: config.source,
+    allFileStats: stats,
+    nbOfFiles: stats.length,
+    nbOfTotalLines: totalLines,
+    averageLinesPerFile: Math.round(totalLines / stats.length),
+    fileSizes: getFileSizes(stats),
+    fileTypes: getFileTypes(stats)
+  }, function(html) {
+    packageFileObject.content = html;
+    callback();
+  });
+};
